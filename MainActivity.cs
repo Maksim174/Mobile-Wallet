767540377f7d44967d2318330f3bd05652421c52
Maksim174 <maksim_sizyurin@mail.ru>
2016-07-14 22:41:25 +0500

    Add files via upload

---
+++ MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using SupportToolbar = Android.Support.V7.Widget.Toolbar;
+using Android.Support.V7.App;
+using Android.Support.V4.Widget;
+using System.Collections.Generic;
+
+namespace Mobile_Wallet
+{
+	[Activity(Label = "Mobile_Wallet", MainLauncher = true, Icon = "@mipmap/icon", Theme="@style/MyTheme")]
+	public class MainActivity : ActionBarActivity
+	{
+		private SupportToolbar mToolbar;
+
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			mToolbar = FindViewById<SupportToolbar>(Resource.Id.toolbar);
+			SetSupportActionBar(mToolbar);
+		}
+	}
+}
+
+
