e1acdd6489e5a75eeba5b7da2b08cc40517c4033
Maksim174 <maksim_sizyurin@mail.ru>
2016-07-18 23:35:52 +0500

    Add files via upload

---
+++ Help.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Widget;
+
+namespace Mobile_Wallet
+{
+	[Activity(Label = "Help")]
+	public class Help : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			SetContentView(Resource.Layout.Help);
+			Button button1 = FindViewById<Button>(Resource.Id.button1);
+			button1.Click += new EventHandler (button1_click);
+			Button button2 = FindViewById<Button>(Resource.Id.button2);
+			button2.Click += new EventHandler (button2_click);
+		}
+			void button1_click(object sender, System.EventArgs e)
+			{
+				StartActivity(typeof(Problem1));
+			}
+			void button2_click(object sender, System.EventArgs e)
+			{
+				StartActivity(typeof(Guess));
+			}
+
+	}
+}
+
---
+++ MainActivity.cs
@@ -0,0 +1,46 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+namespace Mobile_Wallet
+{
+	[Activity(Label = "Mobile_Wallet", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+
+	protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			SetContentView(Resource.Layout.Main);
+			Button button1 = FindViewById<Button>(Resource.Id.button1);
+			button1.Click += new EventHandler(button1_click);
+			Button button2 = FindViewById<Button>(Resource.Id.button2);
+			button2.Click += new EventHandler(button2_click);
+			Button button3 = FindViewById<Button>(Resource.Id.button3);
+			button3.Click += new EventHandler(button3_click);
+			Button button4 = FindViewById<Button>(Resource.Id.button4);
+			button4.Click += new EventHandler(button4_click);
+
+		}
+		void button1_click(object sender, System.EventArgs e)
+		{
+			StartActivity(typeof(Cost));
+		}
+		void button2_click(object sender, System.EventArgs e)
+		{
+			StartActivity(typeof(List));
+		}
+		void button3_click(object sender, System.EventArgs e)
+		{
+			StartActivity(typeof(Help));
+		}
+		void button4_click(object sender, System.EventArgs e)
+		{
+			StartActivity(typeof(INFO));
+		}
+	}
+}
+
+
+
